3b001ed78dcb2d1e6bb4b0785ec03881b0c94248
Alxc <alecandres799@gmail.com>
2025-01-14 09:45:29 -0500

    test

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
+import uvicorn
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 from routes import login, register, todo
 
 app = FastAPI()
@@ -9,6 +11,9 @@ app.include_router(login.router)
 app.include_router(register.router)
 
 
-@app.get("/")
-def main():
-    return {"message": "This is API for ToDo list"}
+@app.get("/", status_code=302, include_in_schema=False)
+async def root():
+    return RedirectResponse("/docs")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
